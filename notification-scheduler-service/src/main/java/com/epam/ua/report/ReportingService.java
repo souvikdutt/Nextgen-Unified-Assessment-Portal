41533e2eea13799c55d8d053c949088daba7d55e
SOUVIK DUTTA <66491294+souvikdutt@users.noreply.github.com>
2024-05-14 13:35:26 +0530

    Delete notification-scheduler-service directory

--- notification-scheduler-service/src/main/java/com/epam/ua/report/ReportingService.java
+++
@@ -1,9 +0,0 @@
-package com.epam.ua.report;
-
-public class ReportingService {
-
-    public void generateReportAndSendMail() {
-        ExcelGenerateReport report = new ExcelGenerateReport();
-        report.createExcel();
-    }
-}
